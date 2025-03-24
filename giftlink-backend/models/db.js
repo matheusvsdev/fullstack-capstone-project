942567dba33164bd186bfd228a498c7670180c1f
matheusvsdev <matheusvaldevino1997@gmail.com>
2025-03-24 15:51:23 -0400

    First commit capstone

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -17,12 +17,17 @@ async function connectToDatabase() {
 
     // Task 1: Connect to MongoDB
     // {{insert code}}
+    await client.connect();
+    console.log("Connected successfully to MongoDB");
 
     // Task 2: Connect to database giftDB and store in variable dbInstance
     //{{insert code}}
+    dbInstance = client.db(dbName);
+
 
     // Task 3: Return database instance
     // {{insert code}}
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
